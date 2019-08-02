85a3b06e24dab45deda3e34856c576d5a091fe98
Daniel Noon <supercookies@outlook.com>
2019-08-01 22:51:40 -0500

    enable cors

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-start npm run start:prod
+web npm run start:prod
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  // tslint:disable-next-line:no-console
-  console.log(`Listening on port ${PORT}`);
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
