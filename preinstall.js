8e1177cb088a787424fada4acc9afc7100509b43
Daniel Noon <supercookies@outlook.com>
2019-08-02 22:41:37 -0500

    add check folder exists preinstall

--- preinstall.js
+++ preinstall.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 de.config();
 
 if (process.env.GOOGLE_CONFIG) {
+  if (!fs.existsSync(__dirname + '/config')) {
+    fs.mkdirSync(__dirname + '/config');
+  }
   if (!fs.existsSync(__dirname + '/config/gcp.json')) {
     fs.writeFileSync(__dirname + '/config/gcp.json', process.env.GOOGLE_CONFIG);
   }
